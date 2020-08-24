5d9f8636dd4ab946dc6c937cdd2d803f6b43ed75
tanmen <tanmen@users.noreply.github.com>
2020-08-24 19:28:39 +0000

    deploy: f70220c420b438ffdd32cbfc74e6cb03b0e9b0ff

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2326bdf76e4da16d2812.js"
+    "url": "webpack-runtime-84245d85c4f972e6593a.js"
   },
   {
     "url": "framework-1b33dcd3199c0f928cc8.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f6a90d14a3e8a57cf0696dd333f2f40a"
+    "revision": "a6c90af08a8bf0c24495267830337fe5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
